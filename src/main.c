b1caf635202d4930c7f004a2b3fdfbc753e53035
bengetch <bengetch@gmail.com>
2023-05-09 10:21:01 -0400

    Implement run-length encoding of line information instead of one instruction per line

--- src/main.c
+++ src/main.c
@@ -7,11 +7,8 @@ int main(int argc, const char* argv[]) {
     initChunk(&chunk);
 
     int constant = addConstant(&chunk, 1.2);
-    int constantTwo = addConstant(&chunk, 5);
-    writeChunk(&chunk, OP_CONSTANT, 10);
-    writeChunk(&chunk, constant, 10);
     writeChunk(&chunk, OP_CONSTANT, 123);
-    writeChunk(&chunk, constantTwo, 123);
+    writeChunk(&chunk, constant, 123);
     writeChunk(&chunk, OP_RETURN, 123);
 
     disassembleChunk(&chunk, "test chunk");
