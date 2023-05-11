5a1b82e2f31f0c565833cda750648460f74d7b59
bengetch <bengetch@gmail.com>
2023-05-11 08:06:33 -0400

    15.1 - 15.2

--- src/main.c
+++ src/main.c
@@ -1,17 +1,23 @@
 #include "common.h"
 #include "chunk.h"
 #include "debug.h"
+#include "vm.h"
 
 int main(int argc, const char* argv[]) {
+    initVM();
     Chunk chunk;
     initChunk(&chunk);
 
     for (int i = 0; i < 300; i++) {
         writeConstant(&chunk, i, i);
+        writeChunk(&chunk, OP_NEGATE, i);
     }
+//    writeConstant(&chunk, 7, 123);
+//    writeChunk(&chunk, OP_NEGATE, 123);
     writeChunk(&chunk, OP_RETURN, 123);
 
-    disassembleChunk(&chunk, "test chunk");
+    freeVM();
+    interpret(&chunk);
     freeChunk(&chunk);
 
     return 0;
